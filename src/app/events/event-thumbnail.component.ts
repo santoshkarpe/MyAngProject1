9bcda28aa78d9d2b5ccac1050f490dc806b721ea
santoshkarpe <karpe4u@gmail.com>
2020-05-24 00:58:29 +0530

    4-Handling Null values and ngIf

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,13 +5,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
   selector: 'event-thumbnail',
   template: `
   <div class="well hoverwell thumbnail">
-    <h2>{{inputEvent.name}}</h2>
-    <div>Date: {{inputEvent.date}}</div>
-    <div>Time: {{inputEvent.time}}</div>
-    <div>Price: \${{inputEvent.price}}</div>
-    <div>
-      <span>Location: {{inputEvent.location.address}}</span>
-      <span class="pad-left">{{inputEvent.location.city}}, {{inputEvent.location.country}}</span>
+    <h2>{{inputEvent?.name}}</h2>
+    <div>Date: {{inputEvent?.date}}</div>
+    <div>Time: {{inputEvent?.time}}</div>
+    <div>Price: \${{inputEvent?.price}}</div>
+    <div *ngIf="inputEvent?.location">
+      <span>Location: {{inputEvent?.location?.address}}</span>
+      <span class="pad-left">{{inputEvent?.location?.city}}, {{inputEvent?.location?.country}}</span>
+    </div>
+    <div *ngIf="inputEvent?.onlineUrl">
+      Online URL: {{inputEvent?.onlineUrl}}
     </div>        
   </div>
   `,

4b24c106119466c1998c26a6139380cef4a53c54
santoshkarpe <karpe4u@gmail.com>
2020-05-24 01:55:46 +0530

    4-Added style with ngStyle

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,9 +7,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
   <div class="well hoverwell thumbnail">
     <h2>{{inputEvent?.name}}</h2>
     <div>Date: {{inputEvent?.date}}</div>
-    <!-- <div [class.green]="inputEvent?.time === '8:00 am'" [ngSwitch]="inputEvent?.time"> -->
-    <!-- <div [ngClass]="{green: inputEvent?.time === '8:00 am', bold: inputEvent?.time === '8:00 am'}"     [ngSwitch]="inputEvent?.time"> -->
-    <div [ngClass]="getStartTimeClass()" [ngSwitch]="inputEvent?.time">
+    <!-- <div [style.color]="inputEvent?.time === '8:00 am' ? '#003300' : '#bbb'" [ngSwitch]="inputEvent?.time"> -->
+    <!-- <div [ngStyle]="{'color': inputEvent?.time === '8:00 am' ? '#003300' : '#bbb', 'font-weight': inputEvent?.time === '8:00 am' ? 'bold' : 'normal'}" [ngSwitch]="inputEvent?.time"> -->
+    <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="inputEvent?.time">
        Time: {{inputEvent?.time}}
        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -26,8 +26,6 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
   </div>
   `,
   styles: [`
-     .green {color: #003300 !important; }
-     .bold { font-weight: bold; }
      .thumbnail { min-height:210px; }
      .pad-left { margin-left: 10px; }
      .well div { color: #bbb; }
@@ -37,17 +35,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 export class EventThumbnailComponent {
   @Input() inputEvent:any
   
-  getStartTimeClass() {
-     // 1
-     /* const isEarlyStart = this.inputEvent && this.inputEvent.time === '8:00 am'
-     return {green: isEarlyStart, bold: isEarlyStart} */
-     // 2
-     /* if(this.inputEvent && this.inputEvent.time === '8:00 am')
-       return 'green bold'
-     return '' */
-     // 3
-     if(this.inputEvent && this.inputEvent.time === '8:00 am')
-        return ['green', 'bold']
-     return []
+  getStartTimeStyle():any {
+    if(this.inputEvent && this.inputEvent.time === '8:00 am') 
+       return {'color': '#003300', 'font-weight': 'bold'}
+    return {}
   }
 }

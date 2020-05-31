79de40d7bc037578c41e27a63b2ef1a502e15388
santoshkarpe <karpe4u@gmail.com>
2020-05-31 22:59:22 +0530

    7-Reactive Form - CreateSession

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -16,27 +16,13 @@ import { EventService } from './shared/event.service';
   `]
 })
 
-export class CreateEventComponent implements OnInit {
-   event:any
+export class CreateEventComponent {
+   
    isDirty:boolean = true
    constructor(@Inject(Router) private router: Router,
     @Inject(EventService) private eventService: EventService) {}
 
-   ngOnInit() {
-     this.event = {
-      name: 'Ng Spectacular',
-      date: new Date('9/26/2036'),
-      time: '10:00 am',
-      price: 599.99,
-      location: {
-        address: '1057 DT',
-        city: 'London',
-        country: 'England'
-      },
-      imageUrl: 'http://ngspectacular.com/logo.com',
-      onlineUrl: 'http://ngspectacular.com'
-     }
-   }
+   
    saveEvent(formValues) {
      //console.log(formValues)
      this.eventService.saveEvent(formValues)
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,13 +6,15 @@ import {
   EventsListComponent,
   EventDetailsComponent,
   EventRouteActivator,
-  EventListResolver
+  EventListResolver,
+  CreateSessionComponent
 } from './events/index';
 
 export const appRoutes:Routes = [
   { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
   { path: 'events', component: EventsListComponent, resolve: {eventsR : EventListResolver} },
   { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
+  { path: 'events/session/new', component: CreateSessionComponent},
   { path: '404', component: Error404Component },
   { path: '', redirectTo: '/events', pathMatch: 'full'},
   { path: 'user', loadChildren: './user/user.module#UserModule'}

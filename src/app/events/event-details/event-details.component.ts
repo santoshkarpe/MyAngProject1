1e8c6bf49be730345df70491a6cdedadeeb72340
santoshkarpe <karpe4u@gmail.com>
2020-06-01 14:41:19 +0530

    8-passing data out of a Child Component to Parent component

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { EventService } from '../shared/event.service';
 import { ActivatedRoute } from '@angular/router'
-import { IEvent } from '../shared/event.model';
+import { IEvent, ISession } from '../shared/index';
 
 @Component({
   templateUrl: './event-details.component.html',
   styles: [`
       .container { padding-left: 20px; padding-right:2opx; }
       .event-image { height: 100px; }
+      a {cursor:pointer}
   `]
 })
 
 export class EventDetailsComponent implements OnInit {
   event:IEvent
+  addMode: boolean
 
   constructor(@Inject(EventService) private eventService: EventService,
     @Inject(ActivatedRoute) private route: ActivatedRoute) {}
@@ -22,5 +24,22 @@ export class EventDetailsComponent implements OnInit {
     this.event = this.eventService.getEvent(
       +this.route.snapshot.params['id']);
    
+  }
+
+  addSession() {
+    this.addMode = true
+
+  }
+
+  saveNewSession(session:ISession) {
+    const nextId = Math.max.apply(null,this.event.sessions.map(s => s.id))
+    session.id = nextId + 1
+    this.event.sessions.push(session)
+    this.eventService.updateEvent(this.event)
+    this.addMode = false
+  }
+
+  cancelSession() {
+    this.addMode = false
   }
 }

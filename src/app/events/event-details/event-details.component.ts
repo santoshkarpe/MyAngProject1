39ad8496acfb5c96ccf173e06efcc6b467379c09
santoshkarpe <karpe4u@gmail.com>
2020-06-24 18:51:16 +0530

    12-Routing to the same Component

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { EventService } from '../shared/event.service';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Params } from '@angular/router'
 import { IEvent, ISession } from '../shared/index';
 
 @Component({
@@ -23,8 +23,13 @@ export class EventDetailsComponent implements OnInit {
 
   ngOnInit() {
     //this.event = this.eventService.getEvent(1);
-    this.event = this.eventService.getEvent(
-      +this.route.snapshot.params['id']);
+    //this.event = this.eventService.getEvent(
+     // +this.route.snapshot.params['id']);
+
+     this.route.params.forEach((params: Params) => {
+       this.event = this.eventService.getEvent(+params['id']);
+       this.addMode = false;
+     })
    
   }
 
